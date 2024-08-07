f3092c9ee0bebb25d043f4f240a4d87904f0b1b2
Yasin <ahmadnuryasin0501@gmail.com>
2024-08-07 15:18:04 +0700

    Belajar

---
+++ index.js
@@ -0,0 +1,8 @@
+let nama = "Ahmad Nur yasin";
+let age = 23;
+let university = "UIN Raden Intan Lampung";
+let citizen = "Pesisir Barat";
+
+console.log(`nama saya ${nama} saya berusia ${age} saya dari universitas ${university} dan berasal dari ${citizen}`);
+
+
